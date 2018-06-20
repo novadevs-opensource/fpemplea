1f2a0565c4935ed37bdf117d4475422804422c2b
brunolorente <soybrunolorente@gmail.com>
2018-06-20 14:28:58 +0200

    Applicant profile birth date restriction updated to 75 years.

--- src/AppBundle/Entity/Perfilestudiante.php
+++ src/AppBundle/Entity/Perfilestudiante.php
@@ -116,7 +116,7 @@ class Perfilestudiante
     /**
      * @Assert\NotBlank()
      * @Assert\Range(
-     *      min="-60 years",
+     *      min="-75 years",
      *      minMessage="¿Tu edad es correcta?",
      *      max="-18 years",
      *      maxMessage="applicant.birthdate"
